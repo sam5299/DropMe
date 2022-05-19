80e469c884a7a42d64be7d1975cc64c17b78072b
Omkar Doke <omkardoke99@gmail.com>
2022-05-19 14:44:09 +0530

    Wallet history notificarion after trip completed

--- DropMe_Backend/services/walletHistory.js
+++ DropMe_Backend/services/walletHistory.js
@@ -5,12 +5,17 @@ const {
 
 //method to add new entry in wallet history collection
 async function addNewHistory(body) {
-  return new WalletHistory(body);
+  let newHistoryObj= await new WalletHistory(body);
+  // console.log('====================================');
+  // console.log(newHistoryObj);
+  // console.log('====================================');
+  return await newHistoryObj.save()
 }
 
 //method to get history of user
 async function getUserWalletHistory(userId) {
-  return WalletHistory.find({ User: userId });
+  return WalletHistory.find({ User: userId }).sort({_id:-1});
 }
+
 
 module.exports = { addNewHistory, getUserWalletHistory };
