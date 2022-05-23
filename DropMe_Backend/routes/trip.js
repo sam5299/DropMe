eb823e69e903f3894a9cde7987fb30209c78c617
Omkar Doke <omkardoke99@gmail.com>
2022-05-24 00:22:24 +0530

    To load new data

--- DropMe_Backend/routes/trip.js
+++ DropMe_Backend/routes/trip.js
@@ -137,9 +137,9 @@ router.put("/updateTripStatus", auth, async (req, res) => {
 
 // set rating to raider
 router.put("/setRating", auth, async (req, res) => {
-  console.log("setRating is called");
-  console.log("triprideid:",req.body.tripRideId);
-  console.log("rating:",req.body.rating);
+  console.log("setRating is called..");
+  // console.log("trip ride id:",req.body.tripRideId);
+  // console.log("rating:",req.body.rating);
   let tripRideId = req.body.tripRideId;
   let rating = req.body.rating;
   let addRatingResult = await setRating(tripRideId, rating);
