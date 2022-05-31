43256f089abd33fe749669a24a6e0d3aff78e140
sam5299 <samshinde5299@gmail.com>
2022-06-01 02:44:53 +0530

    to load data

--- DropMe_Frontend/Component/Authentication/Main.js
+++ DropMe_Frontend/Component/Authentication/Main.js
@@ -11,7 +11,7 @@ import Forgot from "./Forgot";
 const Stack = createNativeStackNavigator();
 
 const Main = () => {
-  const url = "http://192.168.31.158:3100";
+  const url = "http://192.168.43.195:3100";
 
   const initialState = {
     userName: null,
